1098cf38c44cd84098c72ed0874380b72477cc21
Youghi <zykov.igor.95@gmail.com>
2024-01-27 21:44:41 +0100

    libreria quasi completata

--- exercises/src/esercizio31Libreria/Book.java
+++ exercises/src/esercizio31Libreria/Book.java
@@ -1,7 +1,5 @@
 package esercizio31Libreria;
 
-import java.io.IOException;
-
 import esercizio31Libreria.GenreEnum.genre;
 
 public class Book {
@@ -19,25 +17,73 @@ public class Book {
 	
 	
 	public void setData() {
-		this.name = libreria.readString("Inserire nome libro:");
-		this.releaseYear = libreria.readInt("Inserire anno dell'edizione:");
+		this.name = libreria.readLine("Inserire nome libro:");
+		setYear();
+		selectGenre();
 		System.out.println("Inserire");
 		author.setData();
-		System.out.println("Scegliere genere:");
-		for (genre gen : genre.values()) {
-			System.out.println(gen);
-		}
-		try {
-			genere = genre.valueOf(libreria.readString(""));			
-		} catch (IOException e) {
-			// TODO: handle exception
-		}
 
 	}
-	
+
 	public void getData() {
 		System.out.println("Titolo: " + name + "\n" + "Anno edizione: " + releaseYear + "\n" + "Genere: " + genere);
 		author.getData();
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public genre getGenere() {
+		return genere;
+	}
+
+	private void setYear() {
+		setReleaseYear(libreria.readInt("Inserire anno dell'edizione:"));
+	}
+
+	public void setGenere(String gen) throws IllegalArgumentException {
+		this.genere = genre.valueOf(gen);
+	}
+
+	public void selectGenre() {
+		System.out.println("Scegliere genere:");
+		for (genre gen : genre.values()) {
+			System.out.println(gen);
+		}
+		Boolean check = false;
+		while (!check) {
+			try {
+				String gen = libreria.readString("");
+				setGenere(gen);
+				check = true;
+			} catch (IllegalArgumentException e) {
+				System.out.println("genere errato, riprovare:");
+				check = false;
+			}
+
+		}
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	
 
 }
--- exercises/src/esercizio31Libreria/LibreriaSystem.java
+++ exercises/src/esercizio31Libreria/LibreriaSystem.java
@@ -1,7 +1,10 @@
 package esercizio31Libreria;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import esercizio31Libreria.GenreEnum.genre;
 
 //Aggiungi un nuovo libro" + "\n"
 //+ "2 - Modifica dati di un libro" + "\n" + "3 - Visualizza tutti i libri" + "\n"
@@ -23,10 +26,14 @@ public class LibreriaSystem {
 	}
 
 	public void viewBook() {
-		for (int i = 0; i < books.size(); i++) {
-			System.out.println("------ Libro n° " + i);
-			books.get(i).getData();
-			System.out.println("");
+		if (books.size() != 0) {
+			for (int i = 0; i < books.size(); i++) {
+				System.out.println("------ Libro n° " + i);
+				books.get(i).getData();
+				System.out.println("");
+			}
+		} else {
+			System.out.println("Non ci sono libri nell'archivio");
 		}
 	}
 
@@ -35,21 +42,123 @@ public class LibreriaSystem {
 	}
 
 	public void searchBook() {
+		int ind = readInt(("Operazioni disponibili: " + "\n" + "1 - ricerca per nome" + "\n" + "2 - ricerca per autore"
+				+ "\n" + "3 - ricerca per genere"));
+		switch (ind) {
+		case 1:
+			String checkName = readLine("Inserire nome libro da cercare:");
+			System.out.println("Risultato ricerca per nome " + checkName + ":");
+			Boolean check = true;
+			for (int i = 0; i < books.size(); i++) {
+				if (books.get(i).getName().equalsIgnoreCase(checkName)) {
+					System.out.println("------ Libro n° " + i);
+					books.get(i).getData();
+					System.out.println("");
+					check = false;
+				}
+			}
+			System.out.println(check == true ? "Non ci sono libri con questo nome" : "");
+			break;
+
+		case 2:
+			String AuthorName = readString("Inserire nome autore da cercare:");
+			String AuthorSurame = readString("Cognome:");
+			System.out.println("Risultato ricerca per autore " + AuthorName + " " + AuthorSurame + ":");
+			check = true;
+			for (int i = 0; i < books.size(); i++) {
+				if (books.get(i).getAuthor().getName().equalsIgnoreCase(AuthorName)
+						&& books.get(i).getAuthor().getSurname().equalsIgnoreCase(AuthorSurame)) {
+					System.out.println("------ Libro n° " + i);
+					books.get(i).getData();
+					System.out.println("");
+					check = false;
+				}
+			}
+			System.out.println(check == true ? "Non ci sono libri di questo autore" : "");
+			break;
+
+		case 3:
+			System.out.println("Scegliere genere libro da cercare:");
+			int code = 0;
+			for (genre gen : genre.values()) {
+				System.out.println(code + ": " + gen);
+				code++;
+			}
+			genre genere = null;
+			switch (readInt("")) {
+			case 0:
+				genere = genre.Romanzo;
+				break;
+			case 1:
+				genere = genre.Biografia;
+				break;
+			case 2:
+				genere = genre.Saggio;
+				break;
+			case 3:
+				genere = genre.Racconto;
+				break;
+			case 4:
+				genere = genre.Didattico;
+				break;
+			}
+			System.out.println("Risultato ricerca per genere " + genere + ":");
+			check = true;
+			for (int i = 0; i < books.size(); i++) {
+				if (books.get(i).getGenere() == genere) {
+					System.out.println("------ Libro n° " + i);
+					books.get(i).getData();
+					System.out.println("");
+					check = false;
+				}
+			}
+			System.out.println(check == true ? "Non ci sono libri di questo genere" : "");
+			break;
+		}
+		System.out.println("");
+		System.out.println("");
 
 	}
 
 	public void deleteBook() {
-
+		if (books.size() != 0) {
+			for (int i = 0; i < books.size(); i++) {
+				System.out.println("------ Libro n° " + i);
+				books.get(i).getData();
+				System.out.println("");
+			}
+			books.remove(readInt("Selezionare numero libro da rimuovere:"));
+		} else {
+			System.out.println("Non ci sono libri nell'archivio");
+		}
 	}
 
 	public String readString(String promt) {
+		System.out.println(promt);
+		return userIn.next();
+	}
+
+	public String readLine(String promt) {
+		userIn.nextLine();
 		System.out.println(promt);
 		return userIn.nextLine();
 	}
 
 	public int readInt(String promt) {
 		System.out.println(promt);
-		return userIn.nextInt();
+		Boolean check = false;
+		int data = 0;
+		while (!check) {
+			try {
+				data = userIn.nextInt();
+				check = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Input errato, riprovare:");
+				check = false;
+				userIn.next();
+			}
+		}
+		return data;
 	}
 
 }
